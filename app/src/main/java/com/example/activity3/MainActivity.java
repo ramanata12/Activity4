9a1a9795a14e44a93246a481e21792279e5690f6
ramanata12 <ramadanawinata@gmail.com>
2022-03-14 15:31:20 +0700

    Membuat menu dan Floating Button pada android

--- app/src/main/java/com/example/activity3/MainActivity.java
+++ app/src/main/java/com/example/activity3/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,5 +79,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "Pendaftaran"
+            Intent i = new Intent(MainActivity.this,Pendaftaran.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
